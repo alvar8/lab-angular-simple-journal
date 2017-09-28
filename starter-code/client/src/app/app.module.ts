3936b85c423282a73c85c895bd64882c4da95a6a
alvar8 <alvarfernandez92@hotmail.com>
2017-09-28 16:51:17 +0200

    iteration2 done

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RetrieveService} from './services/retrieve.service'
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [RetrieveService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ starter-code/client/src/app/entry-list/entry-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { RetrieveService } from '../services/retrieve.service'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/toPromise'
+
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css'],
+})
+export class EntryListComponent implements OnInit {
+  entries;
+  constructor(public retrieve:RetrieveService) { }
+
+  ngOnInit() {
+    this.entries = this.retrieve.getList().subscribe( ey => {
+    // .toPromise().then( ey => {
+      this.entries = ey
+      console.log(this.entries)
+    });
+  }
+
+}
